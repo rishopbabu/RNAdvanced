1c966a8409643ba82edf6f562c812bc7cf9bcd78
Rishop Babu <rishopbabu22@gmail.com>
2023-09-11 13:02:29 +0530

    flatlist with normal data loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import SignUpScreen from './screens/SignUpScreen'
+import FlatListComponent from "./screens/FlatList"
 
 export default function App() {
   return (
   <>
-    <SignUpScreen />
+    {/* <SignUpScreen /> */}
+    <FlatListComponent/>
   </>
   )
 }
