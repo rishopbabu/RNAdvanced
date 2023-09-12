cd27c30cf27ea4d9d63aedc0f835b471c5da6a0c
Rishop Babu <rishopbabu22@gmail.com>
2023-09-12 07:48:10 +0530

    react stak navigation updated

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import SectionListComponent from './screens/SectionList';
 //import FlatListComponentWithScroll from './screens/FlatListWithScroll';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       {/* <SignUpScreen /> */}
       {/* <FlatListComponent /> */}
-      <SectionListComponent />
+      {/* <SectionListComponent /> */}
       {/* <FlatListComponentWithScroll /> */}
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={SectionListComponent} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
